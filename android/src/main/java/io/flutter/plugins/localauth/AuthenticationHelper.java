3987bb3c5bf23a3a7ab0c13e07f271e71473c98a
john-wick <alireza.daryani.ads@gmail.com>
2020-04-29 16:10:58 +0430

    change to orginal data for test

--- android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
+++ android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
@@ -85,7 +85,7 @@ class AuthenticationHelper extends BiometricPrompt.AuthenticationCallback
     this.promptInfo =
         new BiometricPrompt.PromptInfo.Builder()
             .setDescription((String) call.argument("localizedReason"))
-            .setTitle("test change of title")
+            .setTitle((String) call.argument("signInTitle"))
             .setSubtitle((String) call.argument("fingerprintHint"))
             .setNegativeButtonText((String) call.argument("cancelButton"))
             .setConfirmationRequired((Boolean) call.argument("sensitiveTransaction"))
